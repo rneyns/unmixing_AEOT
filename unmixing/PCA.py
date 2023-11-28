283c1f3cedaf1a0e210e735a0417f9b208874bc2
rneyns <56020455+rneyns@users.noreply.github.com>
2023-11-28 11:02:10 +0100

    Update PCA.py
    Now the PCA images will be saved so the students can visualise them in QGIS + the output is a HSI cube now

--- unmixing/PCA.py
+++ unmixing/PCA.py
@@ -5,10 +5,17 @@ import numpy
 from sklearn.decomposition import PCA
 
 # Define input and parameters --> replace PATH with the actual path to the file
-InputImagery = r"PATH"
-OutputFolder = r"PATH"
-NumberComponents = 3
 def extract_PCA(InputImagery, NumberComponents):
+
+  # Read imagery
+  Image = gdal.Open(InputImagery).ReadAsArray()
+  
+  # Get imagery information
+  prj = gdal.Open(InputImagery).GetProjectionRef()
+  extent = gdal.Open(InputImagery).GetGeoTransform()
+  
+  # Adapt shape from (bands, row, col) to (row, col,bands)
+  Image_AdaptedAxisOrder = numpy.moveaxis(Image,0,-1)
   
   # Adapt shape from 3D to 2D; new shape = (row * col, bands)
   Image_2D = Image_AdaptedAxisOrder[:, :, :].reshape((Image.shape[1] * Image.shape[2],Image.shape[0]))
@@ -31,6 +38,19 @@ def extract_PCA(InputImagery, NumberComponents):
   
   # Reshape PCA output
   pca_output_reshape = pca_output.reshape((Image.shape[1],Image.shape[2],NumberComponents))
+
+  # Write output
+  for i in range (0,NumberComponents):
+      OutputArray = pca_output_reshape[:,:,i]
+      
+      # Write output
+      OutputNamePath = os.path.join(OutputFolder,'PCA_PC_'+str(i+1)+'.tif')
+      Output = gdal.GetDriverByName('GTiff').Create(OutputNamePath,Image.shape[2],Image.shape[1],1,gdal.GDT_Float64)
+      Output.SetProjection(prj)
+      Output.SetGeoTransform(extent)
+      Output.GetRasterBand(1).WriteArray(OutputArray)
+      Output.GetRasterBand(1).SetNoDataValue(-9999)
+      del Output
   
-  # Return the resulting image
-  return pca_output_reshape
+  # Return the resulting image as a HSI CUBE
+  return pca_output.reshape((Image.shape[2],Image.shape[1],NumberComponents))
